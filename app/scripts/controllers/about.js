88958e63d733c711e687234f096d221b02d7bcdd
Paolinni <pxv2009@gmail.com>
2015-12-14 12:09:16 -0600

    installed dependencies

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myMvpProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myMvpProjectApp
+ */
+angular.module('myMvpProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myMvpProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myMvpProjectApp
+ */
+angular.module('myMvpProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
