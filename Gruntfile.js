5d32f159a5c0e6a69a33b7c0671d1c8336c4c66d
Paolinni Velazquez <Paolinni@Paolinnis-MacBook-Pro.local>
2016-01-30 15:43:11 -0600

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 // Generated on 2015-12-14 using generator-angular 0.15.1
 'use strict';
 
+
 // # Globbing
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
@@ -8,6 +9,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-install-dependencies');
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
