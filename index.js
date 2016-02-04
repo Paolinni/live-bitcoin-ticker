84ee2b694097f327840d5dd50798565e350a12ba
Paolinni <pxv2009@gmail.com>
2016-02-03 20:03:44 -0600

    testing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "./app"));
+app.use(express.static(__dirname + "./../app"));
 app.use('/bower_components', express.static(__dirname + './bower_components'));
 app.use('/node_modules', express.static(__dirname + './node_modules'))
 
